9131b9b3655e441a004e1ed6d5ecbb0835e9ba4f
Adithya sai <123888728+Adithyasai123@users.noreply.github.com>
2024-02-20 19:36:08 +0530

    Update Topbar.js

--- data-ev/src/Components/Topbar/Topbar.js
+++ data-ev/src/Components/Topbar/Topbar.js
@@ -3,6 +3,8 @@ import "./Topbar.css";
 import main_video from "../../Assets/Videos/main_video.mp4";
 import mobilevideo from "../../Assets/Videos/mobilevideo.mp4";
 
+
+
 const Topbar = () => {
   return (
     <div>
@@ -12,22 +14,6 @@ const Topbar = () => {
             <video width="100%" autoPlay loop muted playsInline className="back-video">
               <source src={main_video} type="video/mp4"/>
             </video>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-lg-5">
-              <div className="premier-text">
-                <span>PREMIER</span><br/>
-                <span>TECHNOLOGY</span><br/>
-                <span>COLLABRATIONS</span>
-              </div>
-              <div className="dive-text">
-                <span>Dive Deeper into Our Partnerships</span>
-              </div>
-            </div>
           </div>
         </div>
       </section>
