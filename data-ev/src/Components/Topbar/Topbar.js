318aeed5da50c7db4bc905a7493ea2b1ec3ee7d0
Adithya sai <123888728+Adithyasai123@users.noreply.github.com>
2024-02-20 19:32:03 +0530

    Update Topbar.js

--- data-ev/src/Components/Topbar/Topbar.js
+++ data-ev/src/Components/Topbar/Topbar.js
@@ -2,31 +2,19 @@ import React from "react";
 import "./Topbar.css";
 import main_video from "../../Assets/Videos/main_video.mp4";
 import mobilevideo from "../../Assets/Videos/mobilevideo.mp4";
+
 const Topbar = () => {
   return (
-<<<<<<< HEAD
-   <div>
-   <section>
-   <div className="full-screen" style={{overflow:"hidden"}}>
-   <div className="d-md-block d-none">
-   <video width="100%" autoPlay loop muted plays-inline className="back-video" >
-   <source src={main_video} type="video/mp4"/>
-  </video>
-   </div>
-   </div></section>
-    
- </div>
-=======
-  <div>
-      
-      <div className="full-screen " style={{overflow:"hidden"}}>
-     <div className="d-md-block d-none">
-     <video width="100%" autoPlay loop muted playsInline >
-      <source src={main_video} type="video/mp4"/>
-     </video>
-     </div>
-      </div>
-<div>
+    <div>
+      <section>
+        <div className="full-screen" style={{overflow:"hidden"}}>
+          <div className="d-md-block d-none">
+            <video width="100%" autoPlay loop muted playsInline className="back-video">
+              <source src={main_video} type="video/mp4"/>
+            </video>
+          </div>
+        </div>
+      </section>
       <section>
         <div className="container">
           <div className="row">
@@ -39,17 +27,11 @@ const Topbar = () => {
               <div className="dive-text">
                 <span>Dive Deeper into Our Partnerships</span>
               </div>
-
             </div>
-
           </div>
-
         </div>
       </section>
-      
     </div>
-  </div>
->>>>>>> ee50f899e31d0c6cd69f58dcb7308966a4ebba77
   );
 };
 
